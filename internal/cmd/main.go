374e8f788c6c683600a69f28b3fbec82755da8e4
anilvpatel21 <anilpatel.iiitm@gmail.com>
2025-03-10 21:09:15 +0530

    stage 2: downloading url

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/anilvpatel21/snapurl/internal/ports"
 	"github.com/anilvpatel21/snapurl/pkg/csv"
+	"github.com/anilvpatel21/snapurl/pkg/downloader"
 )
 
 func main() {
@@ -44,16 +45,64 @@ func main() {
 		close(urlChan)
 	}()
 
-	for url := range urlChan {
-		fmt.Println(url)
+	/* ------ stage 1 done ------- */
+
+	/* ------ stage 2 start ------- */
+	// Channels for passing data between stages
+	contentChan := make(chan string)
+
+	// Initialize downloader dependencies
+	downloader := &downloader.HttpDownloader{
+		Timeout: 10,
 	}
 
-	/* ------ stage 1 done ------- */
+	// Start goroutine to download URLs
+	var downloadWait sync.WaitGroup
+	downloadWait.Add(1)
+	go func() {
+		defer func() {
+			downloadWait.Done()
+		}()
+		downloadURLs(downloader, urlChan, contentChan, &downloadWait)
+	}()
+
+	go func() {
+		downloadWait.Wait()
+		close(contentChan)
+	}()
+
+	for content := range contentChan {
+		fmt.Println(content)
+	}
+
+	/* ------ stage 2 done ------- */
 }
 
 // Read URLs from CSV file line by line
 func startReading(r ports.Reader, readChan chan<- string) {
 	if err := r.ReadLines(readChan); err != nil {
 		log.Fatalf("Error reading: %v", err)
+	}
+}
+
+func downloadURLs(downloader ports.Downloader, urlChan chan string, contentChan chan string, wg *sync.WaitGroup) {
+	semaphore := make(chan struct{}, 50)
+	for url := range urlChan {
+		wg.Add(1)
+
+		go func(url string) {
+			defer func() {
+				wg.Done()
+				<-semaphore
+			}()
+			semaphore <- struct{}{}
+
+			content, err := downloader.Download(url)
+			if err != nil {
+				log.Printf("Error downloading %s: %v", url, err)
+				return
+			}
+			contentChan <- content
+		}(url)
 	}
 }
